e27d645a246b346820e7668809848dea6e170630
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-05 12:41:22 +0530

    largest and smallest in array

--- array.cpp
+++ array.cpp
@@ -34,44 +34,44 @@
     
 // }
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int marks[5];
-//     int  i, n=5;
-//     int smallest = INT8_MAX;
-//     int largest = INT8_MIN;
-//     cout<<"enter the elements:";
-//     for(int i=0;i<5;i++){
-//         cin>>marks[i];
-//     }
-//     for( int i =0;i<n;i++){
-//         smallest = min(marks[i],smallest);
-//         largest = max(marks[i],largest);
-//     }
-//     cout<<"smallest= " << smallest<<endl;
-//     cout<<"largest= "<< largest;
-//     return 0;
-    
-// }
-
 #include<iostream>
 using namespace std;
-void changemark(int marks[],int size){
-    cout<<"in function"<<endl;
-    for(int i=0;i<3;i++){
-        marks[i]=2*marks[i];
-        cout<<marks[i]<<endl;
-    }
-}
 int main(){
-    int marks[3]={38,45,43};
-
-    changemark(marks, 3);
-    cout<<"in main"<<endl;
-    for(int i=0;i<3;i++){
-        cout<<marks[i]<<" ";
-        cout<<endl;
+    int marks[5];
+    int  i, n=5;
+    int smallest = INT8_MAX;
+    int largest = INT8_MIN;
+    cout<<"enter the elements:";
+    for(int i=0;i<5;i++){
+        cin>>marks[i];
     }
+    for( int i =0;i<n;i++){
+        smallest = min(marks[i],smallest);
+        largest = max(marks[i],largest);
+    }
+    cout<<"smallest= " << smallest<<endl;
+    cout<<"largest= "<< largest;
+    return 0;
+    
 }
+
+// #include<iostream>
+// using namespace std;
+// void changemark(int marks[],int size){
+//     cout<<"in function"<<endl;
+//     for(int i=0;i<3;i++){
+//         marks[i]=2*marks[i];
+//         cout<<marks[i]<<endl;
+//     }
+// }
+// int main(){
+//     int marks[3]={38,45,43};
+
+//     changemark(marks, 3);
+//     cout<<"in main"<<endl;
+//     for(int i=0;i<3;i++){
+//         cout<<marks[i]<<" ";
+//         cout<<endl;
+//     }
+// }
 
