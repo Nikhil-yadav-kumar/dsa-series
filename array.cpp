94718f7f9081a3b96db9650650a254148a76487c
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-05 16:03:15 +0530

    linear search  using array

--- array.cpp
+++ array.cpp
@@ -34,26 +34,26 @@
     
 // }
 
-#include<iostream>
-using namespace std;
-int main(){
-    int marks[5];
-    int  i, n=5;
-    int smallest = INT8_MAX;
-    int largest = INT8_MIN;
-    cout<<"enter the elements:";
-    for(int i=0;i<5;i++){
-        cin>>marks[i];
-    }
-    for( int i =0;i<n;i++){
-        smallest = min(marks[i],smallest);
-        largest = max(marks[i],largest);
-    }
-    cout<<"smallest= " << smallest<<endl;
-    cout<<"largest= "<< largest;
-    return 0;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int marks[5];
+//     int  i, n=5;
+//     int smallest = INT8_MAX;
+//     int largest = INT8_MIN;
+//     cout<<"enter the elements:";
+//     for(int i=0;i<5;i++){
+//         cin>>marks[i];
+//     }
+//     for( int i =0;i<n;i++){
+//         smallest = min(marks[i],smallest);
+//         largest = max(marks[i],largest);
+//     }
+//     cout<<"smallest= " << smallest<<endl;
+//     cout<<"largest= "<< largest;
+//     return 0;
     
-}
+// }
 
 // #include<iostream>
 // using namespace std;
@@ -75,3 +75,32 @@ int main(){
 //     }
 // }
 
+#include <iostream>
+using namespace std;
+int lin_search(int arr[],int n, int item){
+    for ( int i =0;i<=6;i++){
+        if (arr[i]==item){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main (){
+    int arr[6];
+    cout<<"enter the elements: ";
+    for(int i =1;i<=6;i++){
+        cin>>arr[i];
+    }
+    int item;
+    cout<<"enter the item to be searched: ";
+    cin>>item;
+    int result=lin_search(arr,6,item);
+    if(result!=-1){
+        cout<<"elements found at index "<<result<<endl;
+    }
+    else {
+        cout<<"elements not found";
+    }
+    return 0;
+}
