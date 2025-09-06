b20edc430a6f54bb935b5689c7959ee72122cb4f
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-06 12:35:21 +0530

    vector started

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    // declaration or syntax of vector
+    // vector<int> vec;
+    // vector<int> vec = {1,2,3};
+    vector<int>vec(3,1);
+    cout<<vec[0]<<endl;
+    cout<<vec[1]<<endl;
+    cout<<vec[2]<<endl;
+
+
+}
