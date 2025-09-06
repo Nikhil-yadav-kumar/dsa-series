2fc7e8b00b4d6dc4e2a7a99d244a0b0329eeefa4
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-06 12:54:28 +0530

    push back function

--- vector.cpp
+++ vector.cpp
@@ -24,19 +24,6 @@
 // }
 
 //vector functions
-#include<iostream>
-#include<vector>
-using namespace std;
-int main(){
-    vector<int> vec={3,4,5,6,7,8};
-    for(int i:vec){
-        cout<<i<<endl;
-    }
-    cout<<"size= "<<vec.size()<<endl; // size functions
-    return 0;
-}
-
-
 // #include<iostream>
 // #include<vector>
 // using namespace std;
@@ -44,13 +31,27 @@ int main(){
 //     vector<int> vec={3,4,5,6,7,8};
 //     for(int i:vec){
 //         cout<<i<<endl;
-//     }    
-//     // cout<<"after push back "<<endl;
+//     }
+//     cout<<"size= "<<vec.size()<<endl; // size functions
+//     return 0;
+// }
 
-//     // vec.push_back(9); // PUSH_BACK function
-//     // for(int i:vec){
-//     //     cout<<i<<endl;
-//     // }
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vec={3,4,5,6,7,8};
+    for(int i:vec){
+        cout<<i<<endl;
+    }    
+    cout<<"after push back "<<endl;
+
+    vec.push_back(9); // PUSH_BACK function
+    for(int i:vec){
+        cout<<i<<endl;
+    }
+}
 //     cout<<"after pop back "<<endl;
 
 //     vec.pop_back(); // POP_BACK function
