f5c9518c8a4f399cdfcde96596aeecdb4e1ddb17
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-06 12:40:42 +0530

    for each loop in vector

--- vector.cpp
+++ vector.cpp
@@ -1,14 +1,24 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     // declaration or syntax of vector
+//     // vector<int> vec;
+//     // vector<int> vec = {1,2,3};
+//     vector<int>vec(3,1);
+//     cout<<vec[0]<<endl;
+//     cout<<vec[1]<<endl;
+//     cout<<vec[2]<<endl;
+
+
+// }
+//for Each loop
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    // declaration or syntax of vector
-    // vector<int> vec;
-    // vector<int> vec = {1,2,3};
-    vector<int>vec(3,1);
-    cout<<vec[0]<<endl;
-    cout<<vec[1]<<endl;
-    cout<<vec[2]<<endl;
-
-
+    vector<char>vec={'a','b','c','d','e'};
+    for( char i : vec){
+        cout<< i<<endl;
+    }
 }
